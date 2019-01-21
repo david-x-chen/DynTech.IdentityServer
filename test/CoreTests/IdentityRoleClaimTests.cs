cb82cd11bef13d7a1c4f88ec866be13f1bb0ae92
David Chen <davidchen@d-techpro.net>
2019-01-21 23:12:03 +0800

    new tests

---
+++ test/CoreTests/IdentityRoleClaimTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Tests
+{
+	using System.Security.Claims;
+	using Microsoft.AspNetCore.Identity.MongoDB;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class IdentityRoleClaimTests : AssertionHelper
+	{
+		[Test]
+		public void Create_FromClaim_SetsTypeAndValue()
+		{
+			var claim = new Claim("type", "value");
+
+			var roleClaim = new IdentityRoleClaim(claim);
+
+			Expect(roleClaim.Type, Is.EqualTo("type"));
+			Expect(roleClaim.Value, Is.EqualTo("value"));
+		}
+
+		[Test]
+		public void ToSecurityClaim_SetsTypeAndValue()
+		{
+			var roleClaim = new IdentityRoleClaim {Type = "t", Value = "v"};
+
+			var claim = roleClaim.ToSecurityClaim();
+
+			Expect(claim.Type, Is.EqualTo("t"));
+			Expect(claim.Value, Is.EqualTo("v"));
+		}
+	}
+}
--- test/CoreTests/IdentityRoleTests.cs
+++ test/CoreTests/IdentityRoleTests.cs
@@ -1,6 +1,8 @@
 ﻿namespace Tests
 {
-	using Microsoft.AspNetCore.Identity.MongoDB;
+	using System.Linq;
+    using System.Security.Claims;
+    using Microsoft.AspNetCore.Identity.MongoDB;
 	using MongoDB.Bson;
 	using NUnit.Framework;
 
@@ -39,6 +41,16 @@
 		}
 
 		[Test]
+		public void Role_To_String()
+		{
+			var name = "admin";
+
+			var role = new IdentityRole(name);
+
+			Expect(role.ToString(), Is.EqualTo(name));
+		}
+
+		[Test]
 		public void Create_WithRoleName_SetsId()
 		{
 			var role = new IdentityRole("admin");
@@ -46,6 +58,49 @@
 			var parsed = role.Id.SafeParseObjectId();
 			Expect(parsed, Is.Not.Null);
 			Expect(parsed, Is.Not.EqualTo(ObjectId.Empty));
+		}
+
+		[Test]
+		public void Add_New_Role_Claim()
+		{
+			var role = new IdentityRole("admin");
+			role.AddClaim(new Claim ("edit", "edit"));
+
+			var existingClaim = role.Claims.Any(c => c.Type == "edit");
+			Expect(existingClaim, Is.True);
+		}
+
+		[Test]
+		public void Add_New_Role_Claim_Then_Remove()
+		{
+			var role = new IdentityRole("admin");
+			var claim = new Claim ("edit", "edit");
+			role.AddClaim(claim);
+
+			var existingClaim = role.Claims.Any(c => c.Type == claim.Type);
+			Expect(existingClaim, Is.True);
+
+			role.RemoveClaim(claim);
+
+			existingClaim = role.Claims.Any(c => c.Type == claim.Type);
+			Expect(existingClaim, Is.False);
+		}
+
+		[Test]
+		public void Replace_Role_Claim()
+		{
+			var role = new IdentityRole("admin");
+			var claim = new Claim ("edit", "edit");
+			role.AddClaim(claim);
+
+			var existingClaim = role.Claims.Any(c => c.Type == claim.Type);
+			Expect(existingClaim, Is.True);
+
+			var newClaim = new Claim("edit_all", "all");
+			role.ReplaceClaim(claim, newClaim);
+
+			existingClaim = role.Claims.Any(c => c.Type == newClaim.Type);
+			Expect(existingClaim, Is.True);
 		}
 	}
 }
